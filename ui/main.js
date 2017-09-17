3ad088dc1f8bf16307dbee45c16f94f00872eda4
rakadaaku <rakeshbhatt888@gmail.com>
2017-09-17 21:36:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //change the text
 var element = document.getElementById('main-text');
 element.innerHTML = 'new value' ;
+
+//moving image
+var element = document.getElementById('madi');
+madi.onclick = function () {
+    madi.style.marginLeft = '100px';
+};
