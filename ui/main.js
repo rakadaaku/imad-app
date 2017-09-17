07a0be57783135972a17d4aa5f21b3f1186f9cba
rakadaaku <rakeshbhatt888@gmail.com>
2017-09-17 21:52:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,11 @@ element.innerHTML = 'new value' ;
 
 //moving image
 var element = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10 ;
+    madi.style.marginLeft = marginLeft + 'px';
+}
 madi.onclick = function () {
-    madi.style.marginLeft = '100px';
+    var interval = setInterval(moveRight,100);
 };
